80df38b92d669e32ea1e13155e07acd2f1165007
emiferreyra89 <ferreyra89@gmail.com>
2023-12-05 18:58:41 -0300

    Renderize la pagina Registro con ejs y agregue codigo a su respectivo controlador

--- UPStables-2.0/src/controllers/usersController.js
+++ UPStables-2.0/src/controllers/usersController.js
@@ -1,10 +1,41 @@
+const subtitulo = "registrate";
+const formRegistro = ['NoMBre','Apellido','Domicilio','email','ConTRaseña','confiRMAR contraseña'];
+
+function tipo(element) {
+  switch (element.toLowerCase()) {
+
+    case "email":
+      type = "email";
+      break;
+
+    case "contraseña":
+      type = "password";
+      break;
+
+    case "confirmar contraseña":
+      type = "password";
+      break;
+
+    default:
+      type = "text";
+      break;
+  };
+
+  return type;
+}
+
+ function etiqueta(element) {
+   let palabraCapital = element.toUpperCase().charAt(0) + element.substring(1, element.length).toLowerCase();
+   return palabraCapital;
+ }
+
 const userController = {
     register: function(req, res, next) {
-        res.render('/users/register', { title: 'Registro' });
+        res.render('/users/register', { title: 'Registro', subtitulo,formRegistro,tipo,etiqueta});
       },
 
     createUser: function(req, res, next) {
-        res.render('index', { title: 'Home' });
+        res.redirect('index', { title: 'Home' });
       },
 
     login: function(req, res, next) {
