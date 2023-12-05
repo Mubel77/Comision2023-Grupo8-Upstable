a64e6df243649c6ad50077f851419c7b6144702f
emiferreyra89 <ferreyra89@gmail.com>
2023-12-04 22:44:20 -0300

    Reordene la primera estructura del proyecto despues de usar express-generator

---
+++ UPStables-1.0/app.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
+app.post("/login",(req,res) => {
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+app.post("/register",(req,res) => {
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.get("/productDetail", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+});
+app.get("/productCart", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"));
+});
+app.use(express.static("public"));
+
+app.listen(port, console.log(`sever running in http://localhost:${port}`));
