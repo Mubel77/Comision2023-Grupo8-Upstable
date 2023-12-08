31f77c1818526219ee4ea08865b2bd6750d328cb
emiferreyra89 <ferreyra89@gmail.com>
2023-12-07 23:01:41 -0300

    Modifique la ruta de los botones

--- UPStables-2.0/src/controllers/productsController.js
+++ UPStables-2.0/src/controllers/productsController.js
@@ -1,4 +1,8 @@
 const formRegistro=['id','nombre','marca','modelo','descripcion','precio','imagen']
+const fs = require("fs");
+const path = require("path");
+const json = fs.readFileSync(path.join(__dirname,"../database/productos.json"),"utf-8")
+const productos = JSON.parse(json);
 
 const productsController = {
     detail: function(req, res, next) {
