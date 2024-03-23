79cb2434a726c2661c21546636fa3712aba6e59d
emiferreyra89 <ferreyra89@gmail.com>
2024-03-23 00:37:48 -0300

    Agregue el archivo uploadAvatarUser.js a la carpeta middlewares

---
+++ UPStables-2.0/src/middlewares/uploadAvatarUser.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname,'/../../public/images/users'))
+      },
+      filename: (req, file, cb) => {
+        const avatarFilename='Avatar-User'+ Date.now() + path.extname(file.originalname);
+        cb(null, avatarFilename)
+      }
+});
+
+const fileFilter = (req, file, cb) => {
+    const filtro = /\.(jpg|jpeg|png|gif)$/;
+    if (filtro.test(file.originalname)) {
+      cb(null, true);
+    } else {
+      req.errorValidationImage = "No es un tipo de archivo válido";
+      cb(null, false);
+    }
+  };
+  
+
+module.exports = multer({storage},fileFilter)
