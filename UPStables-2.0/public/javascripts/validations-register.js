c1e6e976e494c3593d911b64e600690deb1181cf
ssantiBarrioss <santiago.barrios@hotmail.es>
2024-04-01 01:34:44 -0300

    subo validaciones del lado del front

--- UPStables-2.0/public/javascripts/validations-register.js
+++ UPStables-2.0/public/javascripts/validations-register.js
@@ -15,73 +15,16 @@ window.onload = function(){
     // const msg = document.querySelectorAll("form p");
     const inputs = document.querySelectorAll(".div_config input");
     const msg= document.querySelectorAll(".div_config p")
-    
-    let emailVal = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    const expRegLetter = /^[A-Za-z ]+$/
+    const emailVal = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     let sinError= false
     let error; 
     const email = variables.email
 
-    // email.addEventListener("blur",(e)=>{
-        
-    //     if(!emailVal.test(email.value)){
-    //         email.style.borderColor = "red"
-    //         error = document.createElement("p");
-    //         error.innerHTML = "Email inválido"
-    //         sinError = false
-    //         console.log("EMAIL:",sinError)
-    //     };
-    //     email.onchange = function(e){
-    //         email.style.borderColor = "#2DD4DA"
-    //         sinError = true
-    //         console.log("CAMBIO EMAIL:",sinError)
-    //         error.innerHTML = ""
-    //     }   
-    // })
-   
-    // email.addEventListener("blur", () => {
-    //     if (!emailVal.test(email.value)) {
-    //         console.log("email no valido")
-    //         email.style.borderColor = "red";
-    //         error = document.createElement("p");
-    //         error.innerHTML = "Email inválido";
-    //         email.parentNode.insertBefore(error, email.nextSibling);
-    //     } else {
-    //         email.style.borderColor = "#2DD4DA";
-    //         const error = email.nextSibling;
-    //         if (error && error.tagName === "P") {
-    //             error.remove();
-    //         }
-    //     }
-    // });
-
-    // const inpute= [];
-    // inputs.forEach(input => {
-    //     inpute.push(input.placeholder)
-        
-    //     input.addEventListener('blur', () => {
-            
-    //         if(input.value == ""){
-    //             console.log("se ejecuta blur");
-    //             error = document.createElement("p");
-    //             error.innerHTML = `Debes completar con ${input.placeholder}`;
-                
-    //             input.parentNode.insertBefore(error, input.nextSibling);
-    //         }else{
-    //            if(error){
-    //             error.innerHTML = "";
-    //             sinError = true
-    //            }
-    //         }
-            
-
-           
-    //     });
-
-    // });
-    
+  
     function mostrarError(input, mensaje) {
-        const errorCont = input.parentElement.querySelector('.error-mensaje');
-        if (!errorCont) {
+        const errorP = input.parentElement.querySelector('.error-mensaje');
+        if (!errorP) {
             const error = document.createElement("p");
             error.className = 'error-mensaje';
             error.innerHTML = mensaje;
@@ -90,14 +33,14 @@ window.onload = function(){
     }
     
     function ocultarError(input) {
-        const errorCont = input.parentElement.querySelector('.error-mensaje');
-        if (errorCont) {
-            errorCont.remove();
+        const errorP = input.parentElement.querySelector('.error-mensaje');
+        if (errorP) {
+            errorP.remove();
         }
     }
-    const inpute= [];
+    const nombreInput= [];
     inputs.forEach(input => {
-        inpute.push(input.placeholder)
+        nombreInput.push(input.placeholder)
         input.addEventListener('blur', () => {
             if (input.value.trim() === "") {
                 input.style.borderColor = "red"
@@ -108,6 +51,16 @@ window.onload = function(){
                 input.style.borderColor = "#2DD4DA"
                 if (input === variables.email && !emailVal.test(input.value)) {
                     mostrarError(input, "Email inválido");
+                    input.style.borderColor = "red"
+                    sinError = false;
+                }
+                if (input === variables.nombre && !expRegLetter.test(input.value)) {
+                    mostrarError(input, "Sólo se pueden ingresar letras");
+                    input.style.borderColor = "red"
+                    sinError = false;
+                }
+                if (input === variables.apellido && !expRegLetter.test(input.value)) {
+                    mostrarError(input, "Sólo se pueden ingresar letras");
                     input.style.borderColor = "red"
                     sinError = false;
                 }
@@ -141,10 +94,13 @@ window.onload = function(){
         });
             console.log("valInput:",valInputs)
              valInputs.forEach((valorInput, index) => {
-            if (valorInput === "") {
-                msg[index].innerHTML = `Debes completar con tu ${inpute[index]}`;
-                sinError = false
-            };
+                
+                    if (valorInput === "") {
+                        msg[index].innerHTML = `Debes completar con tu ${nombreInput[index]}`;
+                        sinError = false
+                    };
+            
+             
         });
            inputs.forEach((input, index) => {
         input.addEventListener('input', () => {
@@ -162,7 +118,66 @@ window.onload = function(){
             
         
         
-       }
+       } 
+       
+       // email.addEventListener("blur",(e)=>{
+        
+    //     if(!emailVal.test(email.value)){
+    //         email.style.borderColor = "red"
+    //         error = document.createElement("p");
+    //         error.innerHTML = "Email inválido"
+    //         sinError = false
+    //         console.log("EMAIL:",sinError)
+    //     };
+    //     email.onchange = function(e){
+    //         email.style.borderColor = "#2DD4DA"
+    //         sinError = true
+    //         console.log("CAMBIO EMAIL:",sinError)
+    //         error.innerHTML = ""
+    //     }   
+    // })
+   
+    // email.addEventListener("blur", () => {
+    //     if (!emailVal.test(email.value)) {
+    //         console.log("email no valido")
+    //         email.style.borderColor = "red";
+    //         error = document.createElement("p");
+    //         error.innerHTML = "Email inválido";
+    //         email.parentNode.insertBefore(error, email.nextSibling);
+    //     } else {
+    //         email.style.borderColor = "#2DD4DA";
+    //         const error = email.nextSibling;
+    //         if (error && error.tagName === "P") {
+    //             error.remove();
+    //         }
+    //     }
+    // });
+
+    // const nombreInput= [];
+    // inputs.forEach(input => {
+    //     inpute.push(input.placeholder)
+        
+    //     input.addEventListener('blur', () => {
+            
+    //         if(input.value == ""){
+    //             console.log("se ejecuta blur");
+    //             error = document.createElement("p");
+    //             error.innerHTML = `Debes completar con ${input.placeholder}`;
+                
+    //             input.parentNode.insertBefore(error, input.nextSibling);
+    //         }else{
+    //            if(error){
+    //             error.innerHTML = "";
+    //             sinError = true
+    //            }
+    //         }
+            
+
+           
+    //     });
+
+    // });
+    
     // inputs.forEach(el => {  
     //  input.push(el.placeholder)
     //  for (const key in variables) {
