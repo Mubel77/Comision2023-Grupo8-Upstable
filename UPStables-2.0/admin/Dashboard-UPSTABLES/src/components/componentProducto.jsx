45a52e81ad120dd7d7d64c9465d2a3cf63cc4092
Maurico <abel772003@gmail.com>
2024-04-11 22:03:43 -0300

    logro mostrar la lista del producto

--- UPStables-2.0/admin/Dashboard-UPSTABLES/src/components/componentProducto.jsx
+++ UPStables-2.0/admin/Dashboard-UPSTABLES/src/components/componentProducto.jsx
@@ -2,20 +2,45 @@ import PropTypes from 'prop-types'
 
 function Producto (props) {
     Producto.propTypes = {
-        modelo: PropTypes.string ,
-        marca:PropTypes.string ,
-        pontencia: PropTypes.number,
-    }
+        modelo: PropTypes.string,
+        marca: PropTypes.string,
+        potencia: PropTypes.number,
+        tomas: PropTypes.number,
+        precio: PropTypes.number,
+        descuento: PropTypes.number,
+        stock: PropTypes.number,
+        imagenes: PropTypes.arrayOf(
+            PropTypes.shape({
+                nombre: PropTypes.string,
+                ubicacion: PropTypes.string
+            })
+        )
+    };
     Producto.defaultProps = {
-        modelo:  'modelo',
-        marca: 'marca',
-        pontencia: 0
-    }
-    return(<>
-    <p>{props.modelo}</p>
-    <p>{props.marca}</p>
-    <p>{props.pontencia}</p>
-    </>)
+        modelo: 'Modelo',
+        marca: 'Marca',
+        potencia: 0,
+        tomas: 0,
+        precio: 0,
+        descuento: 0,
+        stock: 0,
+        imagenes: []
+    };
+    return (
+        <div className="producto">
+            <p>Modelo: {props.modelo}</p>
+            <p>Marca: {props.marca}</p>
+            <p>Potencia: {props.potencia}</p>
+            <p>Tomas: {props.tomas}</p>
+            <p>Precio: ${props.precio}</p>
+            <p>Descuento: {props.descuento ? props.descuento : 0}</p>
+            <p>Stock: {props.stock}</p>
+            {props.imagenes && props.imagenes.length > 0 && (
+                <img src={`http://localhost:3000${props.imagenes[0].ubicacion}/${props.imagenes[0].nombre}`} alt="Imagen de Producto" />
+            )}
+        </div>
+    );
+
 }
 
 export default Producto;
--- UPStables-2.0/admin/Dashboard-UPSTABLES/src/components/componentsListProduct.jsx
+++ UPStables-2.0/admin/Dashboard-UPSTABLES/src/components/componentsListProduct.jsx
@@ -1,77 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import Producto from './componentProducto';
+import '../../../../../UPStables-2.0/public/stylesheets/dashboard.css';
 
 const Product = () => {
-    const [products, setProducts] = useState({modelo:'',marca:'',potencia:''});
+    const [products, setProducts] = useState({ productos: [] });
 
     useEffect(() => {
         fetch('http://localhost:3000/products/api/list')
             .then(response => response.json())
             .then(data => {
-                // console.log(response,'holaaa');
-                setProducts(data);
+                setProducts({ ...products, productos: data.productos });
             })
             .catch(error => {
                 console.log(error);
             });
     }, []);
-console.log( products);
-    return (
-        <>
-      {/* <div className="list-entidades">
-        <ul>
-          {products.productos.map((item, i) => (
-            <li className="box_li" key={i + item}>
-              < Producto 
-              modelo={item.modelo}
-              marca={item.marcas.marca}
-              potencia={item.potencia}
-              />
-            </li>
-          ))}
-        </ul>
-      </div> */}
-      
-    </>
-        // <main className="dashboard_main">
-        //     <div className="dashboard_box_search-add">
-        //         <a href="/products"><button className="dashboard_add">List Products</button></a>
-        //         <div className="dashboard_search">
-        //             <form action="/products/dashboard/search" method="GET">
-        //                 <input type="text" name="keywords" placeholder="Buscar producto por Marca"/>
-        //                 <button type="submit"><i className="fas fa-search"></i></button>
-        //             </form>
-        //         </div>
-        //         <a href="/products/formCreate"><button className="dashboard_add">+ Agregar Producto</button></a>
-        //     </div>
 
-        //     <section className="dashboard_section">
-        //         {products.productos.map(producto => (
-        //             <article key={producto.id} className="dashboard_article">
-        //                 <div className="dashboard_div_img">
-        //                     <img src={producto.imagen} alt=""/>
-        //                 </div>
-        //                 <div className="dashboard_div_texto">
-        //                     <h2>Modelo:  {producto.modelo}</h2>
-        //                     <h3>Marca:  {producto.marcas.marca}</h3>
-        //                     <h4>Categoria:  {producto.categorias.categoria}</h4>
-        //                     <p> {producto.descripcion}</p>
-        //                     <h4>Potencia:  {producto.potencia}</h4>
-        //                     <h4>Tomas:  {producto.tomas}</h4>
-        //                     <h4>Precio: $  {producto.precio}</h4>
-        //                     <h4>Descuento:  {producto.descuento ? producto.descuento : 0} </h4>
-        //                     <h4>Stock:  {producto.stock}</h4>
-        //                 </div>
-        //                 <div className="dashboard_div_botones">
-        //                     <a href={`/products/formUpdate/${producto.id}`}><button>Modificar</button></a>
-        //                     <form action={`/products/delete/${producto.id}?_method=DELETE`} method="POST">
-        //                         <button type="submit">Eliminar</button>
-        //                     </form>
-        //                 </div>
-        //             </article>
-        //         ))}
-        //     </section>
-        // </main>
-    )
+    return (
+        <main className="dashboard_main">
+            <div className="dashboard_box_search-add">
+                <a href="/products"><button className="dashboard_add">List Products</button></a>
+                <div className="dashboard_search">
+                    <form action="/products/dashboard/search" method="GET">
+                        <input type="text" name="keywords" placeholder="Buscar producto por Marca" />
+                        <button type="submit"><i className="fas fa-search"></i></button>
+                    </form>
+                </div>
+                <a href="/products/formCreate"><button className="dashboard_add">+ Agregar Producto</button></a>
+            </div>
+
+            <section className="dashboard_section">
+                <div className="list-entidades">
+                    <ul>
+                        {products.productos.map((item, i) => (
+                            <li className="box_li" key={i + item}>
+                                <div className="dashboard_div_texto">
+                                    <Producto
+                                        modelo={item.modelo}
+                                        marca={item.marcas.marca}
+                                        potencia={item.potencia}
+                                        imagenes={item.imagenes}
+                                        tomas={item.tomas}
+                                        precio={item.precio}
+                                        descuento={item.descuento}
+                                        stock={item.stock}
+                                        key={i}
+                                    />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </section>
+        </main>
+    );
 };
 
 export default Product;
