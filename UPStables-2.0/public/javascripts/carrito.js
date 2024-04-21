8c14893a00a44b020d076f1483559ffff682939d
Maurico <abel772003@gmail.com>
2024-04-20 21:06:29 -0300

    guardo cambios

--- UPStables-2.0/public/javascripts/carrito.js
+++ UPStables-2.0/public/javascripts/carrito.js
@@ -1,70 +1,70 @@
-window.onload = function() {
-    const botonMas = document.getElementById('botonMas');
-    const botonMenos = document.getElementById('botonMenos');
-    const botonCarrito = document.getElementById('botonCarrito'); 
-    const numeroUnidadesElemento = document.getElementById('numeroUnidades');
-    const cantUnidades = numeroUnidadesElemento.innerText;
-    const contadorCarritoElemento = document.getElementById('cartItemCount');
-    const idProducto = document.getElementById('idProducto') 
-    const id = idProducto.innerText
+// window.onload = function() {
+//     const botonMas = document.getElementById('botonMas');
+//     const botonMenos = document.getElementById('botonMenos');
+//     const botonCarrito = document.getElementById('botonCarrito'); 
+//     const numeroUnidadesElemento = document.getElementById('numeroUnidades');
+//     const cantUnidades = numeroUnidadesElemento.innerText;
+//     const contadorCarritoElemento = document.getElementById('cartItemCount');
+//     const idProducto = document.getElementById('idProducto') 
+//     const id = idProducto.innerText
 
 
-    let numeroUnidades = 1;
-    let contadorCarrito = 0; 
-    let botonCarritoPresionado = false;
+//     let numeroUnidades = 1;
+//     let contadorCarrito = 0; 
+//     let botonCarritoPresionado = false;
 
-    function cargarProduto(e){
-        e.preventDefault();
-        if(e.target.classlist.contains('botonCarrito')){
-            const producto = e.target.parentElement.parentElement;
-            // this.leerDatoProducto(producto)
-            console.log(producto);
-        }
-    }
-let cantidad
-    function actualizarNumeroUnidades() {
-        numeroUnidadesElemento.textContent = numeroUnidades + ' unidad';
-        if (numeroUnidades !== 1) {
-            numeroUnidadesElemento.textContent = numeroUnidades + ' unidades';
-        }
-       cantidad=numeroUnidadesElemento.innerText
-        console.log(numeroUnidadesElemento.innerText, 'wwwww');
-        actualizarContadorCarrito();
-    }
+//     function cargarProduto(e){
+//         e.preventDefault();
+//         if(e.target.classlist.contains('botonCarrito')){
+//             const producto = e.target.parentElement.parentElement;
+//             // this.leerDatoProducto(producto)
+//             console.log(producto);
+//         }
+//     }
+// let cantidad
+//     function actualizarNumeroUnidades() {
+//         numeroUnidadesElemento.textContent = numeroUnidades + ' unidad';
+//         if (numeroUnidades !== 1) {
+//             numeroUnidadesElemento.textContent = numeroUnidades + ' unidades';
+//         }
+//        cantidad=numeroUnidadesElemento.innerText
+//         console.log(numeroUnidadesElemento.innerText, 'wwwww');
+//         actualizarContadorCarrito();
+//     }
 
-    function actualizarContadorCarrito() {
+//     function actualizarContadorCarrito() {
 
-        contadorCarritoElemento.textContent = contadorCarrito;
-    }
+//         contadorCarritoElemento.textContent = contadorCarrito;
+//     }
 
-    botonMas.addEventListener('click', function() {
-        numeroUnidades++;
-        contadorCarrito++;
-        actualizarNumeroUnidades();
-        console.log("Se hizo click en el botón de más",numeroUnidades);
-    });
+//     botonMas.addEventListener('click', function() {
+//         numeroUnidades++;
+//         contadorCarrito++;
+//         actualizarNumeroUnidades();
+//         console.log("Se hizo click en el botón de más",numeroUnidades);
+//     });
 
-    botonMenos.addEventListener('click', function() {
-        if (numeroUnidades > 1) {
-            console.log("Se hizo click en el botón de menos");
-            numeroUnidades--;
-            contadorCarrito--;
-            actualizarNumeroUnidades();
-        }
-    });
+//     botonMenos.addEventListener('click', function() {
+//         if (numeroUnidades > 1) {
+//             console.log("Se hizo click en el botón de menos");
+//             numeroUnidades--;
+//             contadorCarrito--;
+//             actualizarNumeroUnidades();
+//         }
+//     });
 
-    botonCarrito.addEventListener('click', function() {
-        if (!botonCarritoPresionado) { 
-            contadorCarrito++;
-            // console.log("Se hizo click en el botón de más");
-            actualizarContadorCarrito();
-            botonCarritoPresionado = true;
+//     botonCarrito.addEventListener('click', function() {
+//         if (!botonCarritoPresionado) { 
+//             contadorCarrito++;
+//             // console.log("Se hizo click en el botón de más");
+//             actualizarContadorCarrito();
+//             botonCarritoPresionado = true;
 
-        }
-        console.log(cantUnidades,'geeeeeee');
-        localStorage.setItem('id', id)
-        localStorage.setItem('unidades', cantidad)
-    });
+//         }
+//         console.log(cantUnidades,'geeeeeee');
+//         localStorage.setItem('id', id)
+//         localStorage.setItem('unidades', cantidad)
+//     });
 
-    actualizarNumeroUnidades();
-};
+//     actualizarNumeroUnidades();
+// };
